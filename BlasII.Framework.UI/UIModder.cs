96a94951b10a9cf284faca6d652da4d36bef1307
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-10-15 16:42:23 -0400

    ui: add ui modder

---
+++ BlasII.Framework.UI/UIModder.cs
@@ -0,0 +1,78 @@
+﻿using Il2CppTMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BlasII.Framework.UI;
+
+/// <summary>
+/// Provides convenient methods for creating and modifying UI elements
+/// </summary>
+public static class UIModder
+{
+    /// <summary>
+    /// Contains TMP_FontAsset objects
+    /// </summary>
+    public static FontStorage Fonts { get; private set; } = new();
+
+    /// <summary>
+    /// Contains RectTransform objects
+    /// </summary>
+    public static ParentStorage Parents { get; private set; } = new();
+
+    /// <summary>
+    /// Creates a RectTransform with the specified options
+    /// </summary>
+    public static RectTransform Create(RectCreationOptions options)
+    {
+        return new GameObject().AddComponent<RectTransform>().ApplyOptions(options);
+    }
+
+    /// <summary>
+    /// Creates a RectTransform with default options
+    /// </summary>
+    public static RectTransform Create() => Create(new RectCreationOptions());
+
+    /// <summary>
+    /// Contains TMP_FontAsset objects
+    /// </summary>
+    public class FontStorage
+    {
+        /// <summary> Pixelated Blasphemous font </summary>
+        public TMP_FontAsset Blasphemous { get; private set; }
+
+        /// <summary> Standard Arial font </summary>
+        public TMP_FontAsset Arial { get; private set; }
+
+        /// <summary>
+        /// Locates and stores font objects
+        /// </summary>
+        internal void Initialize()
+        {
+            Blasphemous = Object.FindObjectOfType<TextMeshProUGUI>()?.font;
+            Arial = TMP_FontAsset.CreateFontAsset(Resources.GetBuiltinResource<Font>("Arial.ttf"));
+        }
+    }
+
+    /// <summary>
+    /// Contains RectTransform objects
+    /// </summary>
+    public class ParentStorage
+    {
+        /// <summary> Parent of all UI </summary>
+        public Transform Canvas { get; private set; }
+
+        /// <summary> Parent of the main menu UI </summary>
+        public Transform MainMenu => Canvas?.Find("Interfaces/MainMenuWindow_prefab(Clone)");
+
+        /// <summary> Parent of the game logic UI </summary>
+        public Transform GameLogic => Canvas?.Find("InGame/InGameWindow_prefab(Clone)");
+
+        /// <summary>
+        /// Locates and stores transform objects
+        /// </summary>
+        internal void Initialize()
+        {
+            Canvas = Object.FindObjectOfType<CanvasScaler>()?.transform;
+        }
+    }
+}
